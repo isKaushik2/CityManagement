a06104d2e918af0e7929327ea727ce44fa53332d
DARKRAI-yan <raiyancs0076@gmail.com>
2026-04-04 21:14:15 +0600

    added maps

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
